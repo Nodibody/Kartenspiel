464e28cd4ca19f5b7a1d8c7ec926b2023453fc62
Philip-P2205 <50460253+Philip-P2205@users.noreply.github.com>
2020-05-25 18:51:18 +0200

    added startseite

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { KarteComponent } from './karte/karte.component';
 import { DeckComponent } from './deck/deck.component';
+import { StartseiteComponent } from './startseite/startseite.component';
 
 @NgModule({
-  declarations: [AppComponent, KarteComponent, DeckComponent],
+  declarations: [AppComponent, KarteComponent, DeckComponent, StartseiteComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
