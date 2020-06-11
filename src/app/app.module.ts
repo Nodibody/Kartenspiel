dfe24f35019131cd09afb432d0e7b55b8838cb01
Philip-P2205 <50460253+Philip-P2205@users.noreply.github.com>
2020-06-11 15:42:18 +0200

    Startseite

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,45 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ClipboardModule } from '@angular/cdk/clipboard';
+
+import { MatDialogModule } from '@angular/material/dialog';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { KarteComponent } from './karte/karte.component';
 import { DeckComponent } from './deck/deck.component';
 import { StartseiteComponent } from './startseite/startseite.component';
+import { GameComponent } from './game/game.component';
 
 @NgModule({
-  declarations: [AppComponent, KarteComponent, DeckComponent, StartseiteComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    KarteComponent,
+    DeckComponent,
+    StartseiteComponent,
+    GameComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    ClipboardModule,
+    BrowserAnimationsModule,
+    MatDialogModule,
+    MatTooltipModule,
+    MatButtonModule,
+    MatIconModule,
+    MatInputModule,
+    MatFormFieldModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/startseite/startseite.component.ts
+++ src/app/startseite/startseite.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormControl, Validators } from '@angular/forms';
+import { EventEmitter } from 'protractor';
 
 @Component({
   selector: 'app-startseite',
   templateUrl: './startseite.component.html',
-  styleUrls: ['./startseite.component.css']
+  styleUrls: ['./startseite.component.scss'],
 })
 export class StartseiteComponent implements OnInit {
+  sessionIdFC: FormControl = new FormControl('', []);
 
-  constructor() { }
+  constructor(private router: Router) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  createSession() {
+    this.sessionIdFC.markAsUntouched();
   }
 
+  joinSession() {
+    this.sessionIdFC.setErrors({ required: true });
+    this.sessionIdFC.markAsTouched();
+  }
 }
